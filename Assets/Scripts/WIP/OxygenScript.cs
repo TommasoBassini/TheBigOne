734527e02ddb4a65a0d53914d5e492f141014f3a
dbga_31 <dbga_31@dbga.local>
2016-09-16 17:34:03 +0200

    Continuazione meccanica ossigeno: introduzione stato "da fermo" e continuo debugging delle transizioni fra stati (incompleto); necessiterebbe anche di alcuni piccoli accorgimenti...bassa priorità

--- Assets/Scripts/WIP/OxygenScript.cs
+++ Assets/Scripts/WIP/OxygenScript.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//Dovrebbe esserci un bug tra rigenerazione ossigeno e cambio camminata/corsa (devo chiedere una cosa a Michele)
-//Devo ancora fare in modo che, durante l'ispezione di ogetti/terminali/cose, non vi sia consumo di ossigeno (devo chiedere una cosa a Michele)
+//C'è un bug di logica (finezza); ripartire dal camminare (non c'è da riscrivere, solo da verificare la logica) 
 //Devo commentare il codice
 
 public class OxygenScript : MonoBehaviour {
 
 	#region PARAMETERS
-	public bool isInspecting;
+	//public bool characterIsInspecting;
+	public bool characterIsRunning;
 
 	[Header ("Ossigeno Parametri Base")]
 	[Range (0f, 100f)] public float oxygenAmount;
 	[Range (0f, 100f)] public float minOxygenAmount = 0f;
 	[Range (0f, 100f)] public float maxOxygenAmount = 100f;
+
+	[Header ("Ossigeno Parametri da Fermo")]
+	[Range (0f, 100f)] public float oxygenStandingDecadenceSpeed = 10f;
+	[Range (0f, 100f)] public float oxygenStandingDecadenceAmount = 1f;
 
 	[Header ("Ossigeno Parametri Camminata")]
 	[Range (0f, 100f)] public float oxygenWalkingDecadenceSpeed = 5f;
@@ -25,15 +29,17 @@ public class OxygenScript : MonoBehaviour {
 
 	[Header ("Ossigeno Parametri Rigenerazione")]
 	[Range (0f, 100f)] public float oxygenRegenerationSpeed = 0.5f;
-	[Range (0f, 100f)] public float oxygenRegenerationAmount = 10f;
+	[Range (0f, 100f)] public float oxygenRegenerationAmount = 40f;
 	#endregion
 
 
 	#region MONOBEHAVIOUR_METHODS
 	public void Start () {
 
+		this.characterIsRunning = false;
+
 		this.OxygenRecharge ();
-		this.StartCoroutine_Auto (this.CO_OxygenWalkingDecadence ());
+		this.StartCoroutine_Auto (this.CO_OxygenStandingDecadence ());
 
 	}
 
@@ -42,35 +48,103 @@ public class OxygenScript : MonoBehaviour {
 		if (this.oxygenAmount <= this.minOxygenAmount) {
 
 			this.StopAllCoroutines ();
+			Debug.Log ("Sono morto");
 
-		}
+		} else {
+			
+			if (Input.GetKeyDown (KeyCode.LeftShift)) {
+				
+				this.characterIsRunning = !this.characterIsRunning;
 
-		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			}
 
-			this.oxygenAmount--;
-			this.StopAllCoroutines ();
-			this.StartCoroutine_Auto (this.CO_OxygenRunningDecadence ());
+			if ((Input.GetKey (KeyCode.W) ^ Input.GetKey (KeyCode.S)) || (Input.GetKey (KeyCode.A) ^ Input.GetKey (KeyCode.D))) {
+				
+				if (this.characterIsRunning) {
+					
+					if ((Input.GetKeyDown (KeyCode.W) || Input.GetKeyUp (KeyCode.W)) ^ (Input.GetKeyDown (KeyCode.S) || Input.GetKeyUp (KeyCode.S))) {
+						
+						this.oxygenAmount--;
+						this.StopAllCoroutines ();
+						this.StartCoroutine_Auto (this.CO_OxygenRunningDecadence ());
+						Debug.Log ("Sto correndo in avanti/dietro");
+						
+					}
 
-		}
+					if ((Input.GetKeyDown (KeyCode.A) || Input.GetKeyUp (KeyCode.A)) ^ (Input.GetKeyDown (KeyCode.D) || Input.GetKeyUp (KeyCode.D))) {
 
-		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+						this.oxygenAmount--;
+						this.StopAllCoroutines ();
+						this.StartCoroutine_Auto (this.CO_OxygenRunningDecadence ());
+						Debug.Log ("Sto correndo in sinistra/destra");
 
-			this.oxygenAmount--;
-			this.StopAllCoroutines ();
-			this.StartCoroutine_Auto (this.CO_OxygenWalkingDecadence ());
+					}
+					
+				} else {
+					
+					if (((Input.GetKeyDown (KeyCode.W) || Input.GetKeyUp (KeyCode.W)) ^ (Input.GetKeyDown (KeyCode.S) || Input.GetKeyUp (KeyCode.S))) &&
+						!(Input.GetKey (KeyCode.A) ^ Input.GetKey (KeyCode.D))) {
+						
+						this.oxygenAmount--;
+						this.StopAllCoroutines ();
+						this.StartCoroutine_Auto (this.CO_OxygenWalkingDecadence ());
+						Debug.Log ("Sto camminando in avanti/dietro");
+						
+					}
 
-		}
+					if ((Input.GetKeyDown (KeyCode.A) || Input.GetKeyUp (KeyCode.A)) ^ (Input.GetKeyDown (KeyCode.D) || Input.GetKeyUp (KeyCode.D)) &&
+						!(Input.GetKey (KeyCode.W) ^ Input.GetKey (KeyCode.S))) {
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
+						this.oxygenAmount--;
+						this.StopAllCoroutines ();
+						this.StartCoroutine_Auto (this.CO_OxygenWalkingDecadence ());
+						Debug.Log ("Sto camminando in sinistra/destra");
 
-			this.StartCoroutine_Auto (this.CO_OxygenRegeneration ());
+					}
+					
+				}
+				
+			} else if ((Input.GetKeyDown (KeyCode.W) || Input.GetKeyUp (KeyCode.W)) || (Input.GetKeyDown (KeyCode.S) || Input.GetKeyUp (KeyCode.S)) ||
+				(Input.GetKeyDown (KeyCode.A) || Input.GetKeyUp (KeyCode.A)) || (Input.GetKeyDown (KeyCode.D) || Input.GetKeyUp (KeyCode.D))) {
+
+				this.characterIsRunning = false;
+				this.oxygenAmount--;
+				this.StopAllCoroutines ();
+				this.StartCoroutine_Auto (this.CO_OxygenStandingDecadence ());
+				Debug.Log ("Sono fermo");
+				
+			}
+			
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				
+				//this.StartCoroutine_Auto (this.CO_OxygenRegeneration ());
+				if ((this.oxygenAmount += this.oxygenRegenerationAmount) > this.maxOxygenAmount) {
+
+					this.oxygenAmount = this.maxOxygenAmount;
+					Debug.Log ("Ricarico ossigeno");
+
+				}
+				
+			}
 
 		}
 
 	}
 	#endregion
 
+
 	#region COROUTINES
+	public IEnumerator CO_OxygenStandingDecadence () {
+
+		while (true) {
+
+			yield return new WaitForSeconds (this.oxygenStandingDecadenceSpeed);
+			this.oxygenAmount -= this.oxygenStandingDecadenceAmount;
+
+		}
+
+	}
+
 	public IEnumerator CO_OxygenWalkingDecadence () {
 
 		while (true) {
@@ -93,7 +167,7 @@ public class OxygenScript : MonoBehaviour {
 
 	}
 
-	public IEnumerator CO_OxygenRegeneration () {
+	/*public IEnumerator CO_OxygenRegeneration () {
 
 		int i = 4;
 
@@ -106,8 +180,9 @@ public class OxygenScript : MonoBehaviour {
 
 		} while (--i > 0);
 
-	}
+	}*/
 	#endregion
+
 
 	#region METHODS
 	public void OxygenRecharge (float currentOxygen = 100f) {
