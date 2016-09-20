3ec5d4c36bb78f6c48829099295e724a5aaaf93d
dbga_31 <dbga_31@dbga.local>
2016-09-20 18:10:38 +0200

    Disassociazione torcia/batteria eseguita; si richiede revisione finale degli script

---
+++ Assets/Scripts/WIP/BatteryScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BatteryScript : MonoBehaviour {
+
+	#region BATTERY_PARAMETERS
+	[Header ("Riferimenti")]
+	public Text uiBatteryText;
+	public TorchLightScript torchLightScript;
+
+	[Header ("Batteria Parametri Base - Da 0f ad 8f")]
+	[Range (0f, 8f)] public float batteryEnergyAmount;
+	[Range (0f, 8f)] public float minBatteryEnergyAmount = 0f;
+	[Range (0f, 8f)] public float maxBatteryEnergyAmount = 1f;
+
+	[Header ("Batteria Parametri di Utilizzo - Da 0f ad 8f")]
+	[Range (0f, 8f)] public float batteryEnergyDecadenceSpeed = 2f;
+	[Range (0f, 8f)] public float batteryEnergyDecadenceAmount = 0.02f;
+
+	[Header ("Batteria Parametri Rigenerazione - Da 0f ad 8f")]
+	[Range (0f, 8f)] public float batteryEnergyRegenerationAmount = 0.2f;
+	#endregion
+
+
+	#region BATTERY_PROPERTIES
+	public float BatteryEnergyAmount {
+
+		set {
+
+			if (value > this.maxBatteryEnergyAmount)
+				this.batteryEnergyAmount = this.maxBatteryEnergyAmount;
+			else if (value < this.minBatteryEnergyAmount)
+				this.batteryEnergyAmount = this.minBatteryEnergyAmount;
+			else
+				this.batteryEnergyAmount = value;
+
+			this.torchLightScript.TorchLightIntensityAmount = this.batteryEnergyAmount;
+			this.uiBatteryText.text = (this.batteryEnergyAmount * 100).ToString ("000");
+
+		}
+
+		get {
+
+			return this.batteryEnergyAmount;
+
+		}
+
+	}
+	#endregion
+
+
+	#region BATTERY_MONOBEHAVIOUR_METHODS
+	public void Start () {
+
+		//fermo tutte le coroutine ad ogni riavvio di scena (si sa mai)
+		this.StopAllCoroutines ();
+
+		//Si può assegnare qualsiasi altro valore in caso di salvataggi od altra occasione
+		this.BatteryEnergyAmount = this.maxBatteryEnergyAmount;
+
+	}
+
+	public void Update () {
+
+		if (Input.GetKeyDown (KeyCode.R))
+			this.BatteryEnergyAmount += this.batteryEnergyRegenerationAmount;
+
+	}
+	#endregion
+
+
+	#region BATTERY_COROUTINES
+	public IEnumerator CO_BatteryEnergyDecadence () {
+
+		while (true) {
+
+			yield return new WaitForSeconds (this.batteryEnergyDecadenceSpeed);
+			this.BatteryEnergyAmount -= this.batteryEnergyDecadenceAmount;
+
+		}
+
+	}
+	#endregion
+
+}
--- Assets/Scripts/WIP/TorchLightScript.cs
+++ Assets/Scripts/WIP/TorchLightScript.cs
@@ -1,52 +1,31 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
 public class TorchLightScript : MonoBehaviour {
 
-	#region TORCHLIGHT_AND_BATTERY_PARAMETERS
+	#region TORCHLIGHT_PARAMETERS
 	[Header ("Riferimenti")]
 	public Light torchLight;
-	public Text uiTorchLightText;
+	public BatteryScript batteryScript;
 
 	[Header ("Flag Booleani")]
 	public bool torchLightHasBeenTriggered;
-
-	[Header ("Batteria Parametri Base - Da 0f ad 8f")]
-	[Range (0f, 8f)] public float batteryEnergyAmount;
-	[Range (0f, 8f)] public float minBatteryEnergyAmount = 0f;
-	[Range (0f, 8f)] public float maxBatteryEnergyAmount = 1f;
-
-	[Header ("Batteria Parametri di Utilizzo - Da 0f ad 8f")]
-	[Range (0f, 8f)] public float batteryEnergyDecadenceSpeed = 2f;
-	[Range (0f, 8f)] public float batteryEnergyDecadenceAmount = 0.02f;
-
-	[Header ("Batteria Parametri Rigenerazione - Da 0f ad 8f")]
-	[Range (0f, 8f)] public float batteryEnergyRegenerationAmount = 0.2f;
 	#endregion
 
 
-	#region TORCHLIGHT_AND_BATTERY_PROPERTIES
-	public float BatteryEnergyAmount {
+	#region TORCHLIGHT_PROPERTIES
+	public float TorchLightIntensityAmount {
 
 		set {
 
-			if (value > this.maxBatteryEnergyAmount)
-				this.batteryEnergyAmount = this.maxBatteryEnergyAmount;
-			else if (value < this.minBatteryEnergyAmount)
-				this.batteryEnergyAmount = this.minBatteryEnergyAmount;
-			else
-				this.batteryEnergyAmount = value;
-
-			this.torchLight.intensity = this.batteryEnergyAmount;
-			this.torchLight.bounceIntensity = this.batteryEnergyAmount;
-			this.uiTorchLightText.text = (this.batteryEnergyAmount * 100).ToString ("000");
+			this.torchLight.intensity = this.batteryScript.BatteryEnergyAmount;
+			this.torchLight.bounceIntensity = this.batteryScript.BatteryEnergyAmount;
 
 		}
 
 		get {
 
-			return this.batteryEnergyAmount;
+			return this.torchLight.intensity;
 
 		}
 
@@ -54,7 +33,7 @@ public class TorchLightScript : MonoBehaviour {
 	#endregion
 
 
-	#region TORCHLIGHT_AND_BATTERY_MONOBEHAVIOUR_METHODS
+	#region TORCHLIGHT_MONOBEHAVIOUR_METHODS
 	public void Start () {
 
 		//Solita pulizia dei booleani
@@ -63,9 +42,6 @@ public class TorchLightScript : MonoBehaviour {
 
 		//fermo tutte le coroutine ad ogni riavvio di scena (si sa mai)
 		this.StopAllCoroutines ();
-
-		//Si può assegnare qualsiasi altro valore in caso di salvataggi od altra occasione
-		this.BatteryEnergyAmount = this.maxBatteryEnergyAmount;
 
 	}
 
@@ -81,28 +57,11 @@ public class TorchLightScript : MonoBehaviour {
 
 		if (this.torchLightHasBeenTriggered) {
 			
-			this.StartCoroutine_Auto (this.CO_BatteryEnergyDecadence ());
+			this.StartCoroutine_Auto (this.batteryScript.CO_BatteryEnergyDecadence ());
 			this.torchLightHasBeenTriggered = false;
 
 		} else if (!this.torchLight.enabled)
-			StopAllCoroutines ();
-
-		if (Input.GetKeyDown (KeyCode.R))
-			this.BatteryEnergyAmount += this.batteryEnergyRegenerationAmount;
-
-	}
-	#endregion
-
-
-	#region TORCHLIGHT_AND_BATTERY_COROUTINES
-	public IEnumerator CO_BatteryEnergyDecadence () {
-
-		while (true) {
-
-			yield return new WaitForSeconds (this.batteryEnergyDecadenceSpeed);
-			this.BatteryEnergyAmount -= this.batteryEnergyDecadenceAmount;
-
-		}
+			this.StopAllCoroutines ();
 
 	}
 	#endregion
