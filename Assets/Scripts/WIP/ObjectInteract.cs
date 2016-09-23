9876b4e5535586d102090483b564a8f15eae94d1
dbga_34 <dbga_34@dbga.local>
2016-09-23 12:13:05 +0200

    Interazione Terminali (Da Migliorare)

--- Assets/Scripts/WIP/ObjectInteract.cs
+++ Assets/Scripts/WIP/ObjectInteract.cs
@@ -6,12 +6,16 @@ using UnityEngine.EventSystems;
 using System.Collections.Generic;
 public class ObjectInteract : MonoBehaviour
 {
-
+    private Vector3 cameraPos;
+    private Quaternion cameraRot;
     private Vector3 lastObjPos;
     private Quaternion lastObjRot;
     private bool isPickuble = false;
     private GameObject pickubleObj;
+
+    public bool isInteracting = false;
     public bool isInspecting = false;
+    public bool isTerminal = false;
 
     public int rotationSpeed;
     public Image mirino;
@@ -24,16 +28,19 @@ public class ObjectInteract : MonoBehaviour
 
     public GameObject inspect;
 
+    public Camera uiCamera;
+
     void Start ()
     {
-
+        cameraPos = Camera.main.transform.position;
 	}
 	
 	void Update ()
     {
-        RaycastHit hit;
-        if (!isInspecting)
+        if (!isInteracting)
         {
+            RaycastHit hit;
+
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2.5f))
             {
                 if (hit.collider.tag == "Pickuble")
@@ -46,6 +53,7 @@ public class ObjectInteract : MonoBehaviour
                     Debug.DrawLine(Camera.main.transform.position, hit.collider.transform.position);
                     if (Input.GetKeyUp(KeyCode.Joystick1Button0))
                     {
+                        isInteracting = true;
                         isInspecting = true;
                         lastObjPos = hit.collider.gameObject.transform.position;
                         lastObjRot = hit.collider.gameObject.transform.rotation;
@@ -64,7 +72,19 @@ public class ObjectInteract : MonoBehaviour
 
                 if (hit.collider.tag == "Terminal")
                 {
+                    if (Input.GetKeyUp(KeyCode.Joystick1Button0))
+                    {
+                        isInteracting = true;
+                        isTerminal = true;
 
+                        cameraRot = Camera.main.transform.rotation;
+                        cameraPos = Camera.main.transform.position;
+
+                        Camera.main.transform.position = hit.collider.transform.position + (hit.collider.transform.up * 1f);
+                        Camera.main.transform.LookAt(hit.collider.transform.position + new Vector3(0,0.16f,0));
+                        GetComponent<FirstPersonController>().enabled = false;
+                        hit.collider.transform.GetChild(0).GetChild(1).GetComponent<Button>().Select();
+                    }
                 }
             }
             else
@@ -77,42 +97,57 @@ public class ObjectInteract : MonoBehaviour
 
         }
 
-        if (isInspecting)
+        if (isInteracting)
         {
-            float angH = Input.GetAxis("RightH");
-            float angV = Input.GetAxis("RightV");
-
-            if (Input.GetAxis("RightH") > 0.25f || Input.GetAxis("RightH") < -0.25f)
+            if (isInspecting)
             {
-                pickubleObj.transform.eulerAngles += new Vector3 (0,angH * rotationSpeed, 0) ;
-            }
-            if (Input.GetAxis("RightV") > 0.25f || Input.GetAxis("RightV") < -0.25f)
-            {
-                inspect.transform.Rotate (new Vector3(-angV * rotationSpeed, 0, 0));
-            }
+                float angH = Input.GetAxis("RightH");
+                float angV = Input.GetAxis("RightV");
 
-            if (Input.GetKeyUp(KeyCode.Joystick1Button0) && isInspecting)
-            {
-                GetComponent<FirstPersonController>().enabled = true;
-                pickubleObj.transform.position = lastObjPos;
-                pickubleObj.transform.rotation = lastObjRot;
-                pickubleObj.transform.SetParent(null);
-                pickubleObj = null;
-                isInspecting = false;
-            }
-
-            if (Input.GetKeyUp(KeyCode.Joystick1Button3) && isInspecting)
-            {
-                ObjectInfo objInfo = pickubleObj.GetComponent<ObjectInfo>();
-
-                if (!objInfo.isScan)
+                if (Input.GetAxis("RightH") > 0.25f || Input.GetAxis("RightH") < -0.25f)
                 {
-                    GameObject newButton = Instantiate(button);
-                    HoloObjectInfo holo = newButton.GetComponent<HoloObjectInfo>();
-                    holo.itemPrefab = objInfo.itemPrefab;
-                    holo.itemImage = objInfo.itemImage;
-                    objInfo.isScan = true;
-                    newButton.transform.SetParent(panel.transform);
+                    pickubleObj.transform.eulerAngles += new Vector3(0, angH * rotationSpeed, 0);
+                }
+                if (Input.GetAxis("RightV") > 0.25f || Input.GetAxis("RightV") < -0.25f)
+                {
+                    inspect.transform.Rotate(new Vector3(-angV * rotationSpeed, 0, 0));
+                }
+
+                if (Input.GetKeyUp(KeyCode.Joystick1Button0) && isInspecting)
+                {
+                    GetComponent<FirstPersonController>().enabled = true;
+                    pickubleObj.transform.position = lastObjPos;
+                    pickubleObj.transform.rotation = lastObjRot;
+                    pickubleObj.transform.SetParent(null);
+                    pickubleObj = null;
+                    isInspecting = false;
+                }
+
+                if (Input.GetKeyUp(KeyCode.Joystick1Button3) && isInspecting)
+                {
+                    ObjectInfo objInfo = pickubleObj.GetComponent<ObjectInfo>();
+
+                    if (!objInfo.isScan)
+                    {
+                        GameObject newButton = Instantiate(button);
+                        HoloObjectInfo holo = newButton.GetComponent<HoloObjectInfo>();
+                        holo.itemPrefab = objInfo.itemPrefab;
+                        holo.itemImage = objInfo.itemImage;
+                        objInfo.isScan = true;
+                        newButton.transform.SetParent(panel.transform);
+                    }
+                }
+            }
+
+            if (isTerminal)
+            {
+                if (Input.GetKeyUp(KeyCode.Joystick1Button1))
+                {
+                    GetComponent<FirstPersonController>().enabled = true;
+                    Camera.main.transform.rotation = cameraRot;
+                    Camera.main.transform.position = cameraPos;
+                    isTerminal = false;
+                    isInspecting = false;
                 }
             }
         }
