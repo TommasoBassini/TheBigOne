9f5af8b6be338ac08e9e2271973c47c22850d530
dbga_67 <dbga_67@dbga.local>
2016-09-26 10:33:59 +0200

    Starting with Menu things

---
+++ Assets/Scripts/Matteo/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    //private GameManager gameManager;
+
+    public GameObject pnlAskTutorial, pnlOptions, pnlBack;
+
+    void Start () {
+        //gameManager = FindObjectOfType<GameManager>();
+    }
+	
+	public void AskTutorial()
+    {
+        pnlBack.SetActive(true);
+        pnlAskTutorial.SetActive(true);
+    }
+
+    public void StartGame(bool goTutorial)
+    {
+        //gameManager.SetLevel(1, goTutorial);
+    }
+
+    public void Options()
+    {
+        pnlBack.SetActive(true);
+        pnlOptions.SetActive(true);
+    }
+
+    public void Back()
+    {
+        pnlBack.SetActive(false);
+        pnlAskTutorial.SetActive(false);
+        pnlOptions.SetActive(false);
+    }
+}
