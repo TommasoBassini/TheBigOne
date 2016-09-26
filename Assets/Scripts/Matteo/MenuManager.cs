9b9f82bd9a40af8a8057f0fcb24c7dce176f230b
dbga_67 <dbga_67@dbga.local>
2016-09-26 17:33:49 +0200

    Menu part 1

--- Assets/Scripts/Matteo/MenuManager.cs
+++ Assets/Scripts/Matteo/MenuManager.cs
@@ -1,37 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour {
 
     //private GameManager gameManager;
 
-    public GameObject pnlAskTutorial, pnlOptions, pnlBack;
+    public GameObject imgBgCool, pnlOptions, pnlCredits, pnlAskToQuit, btnContinueGame, btnNewGame;
+    public Sprite[] backgrounds;
+    public int actualBg;
+    public float startWaitTime, waitBetweenBgs;
+    public bool isLooping, hasSaves;
 
     void Start () {
         //gameManager = FindObjectOfType<GameManager>();
+        actualBg = 0;
+        isLooping = false;
+        if (hasSaves)
+        {
+            GetComponentInChildren<EventSystem>().firstSelectedGameObject = btnContinueGame;
+        }
+        else
+        {
+            GetComponentInChildren<EventSystem>().firstSelectedGameObject = btnNewGame;
+        }
+        StartCoroutine("changeBg");
     }
 	
-	public void AskTutorial()
-    {
-        pnlBack.SetActive(true);
-        pnlAskTutorial.SetActive(true);
-    }
 
-    public void StartGame(bool goTutorial)
+    public void StartGame()
     {
-        //gameManager.SetLevel(1, goTutorial);
+        //gameManager.SetLevel(0);
     }
 
     public void Options()
     {
-        pnlBack.SetActive(true);
-        pnlOptions.SetActive(true);
+        if (pnlOptions.activeInHierarchy)
+        {
+            pnlOptions.SetActive(false);
+        }
+        else
+        {
+            pnlOptions.SetActive(true);
+        }
+    }
+
+    public void Credits()
+    {
+        if (pnlCredits.activeInHierarchy)
+        {
+            pnlCredits.SetActive(false);
+        }
+        else
+        {
+            pnlCredits.SetActive(true);
+        }
+    }
+
+    public void Exit()
+    {
+        if (pnlAskToQuit.activeInHierarchy)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            pnlAskToQuit.SetActive(true);
+        }
     }
 
     public void Back()
     {
-        pnlBack.SetActive(false);
-        pnlAskTutorial.SetActive(false);
         pnlOptions.SetActive(false);
+        pnlCredits.SetActive(false);
+        pnlAskToQuit.SetActive(false);
+    }
+
+    IEnumerator changeBg()
+    {
+        if (!isLooping)
+        {
+            yield return new WaitForSeconds(startWaitTime);
+            isLooping = true;
+        }
+        imgBgCool.GetComponent<Image>().sprite = backgrounds[actualBg];
+        actualBg++;
+        if (actualBg == backgrounds.Length)
+        {
+            actualBg = 0;
+        }
+        yield return new WaitForSeconds(waitBetweenBgs);
+        StartCoroutine("changeBg");
+    }
+
+    void Update()
+    {
+        if (isLooping)
+        {
+            imgBgCool.GetComponent<Image>().color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, Mathf.PingPong((Time.time - startWaitTime) / (waitBetweenBgs / 2), 1));
+        }
     }
 }
