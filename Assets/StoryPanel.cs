86c310b5124a5056ea46b56e14881de697b74c1d
dbga_34 <dbga_34@dbga.local>
2016-09-30 17:41:06 +0200

    terminali storia (da completare)

---
+++ Assets/StoryPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryPanel : MonoBehaviour
+{
+    public TextAsset[] texts;
+    public Text textBox;
+
+
+	void Update ()
+    {
+	
+	}
+
+    public void ShowText(int n)
+    {
+        textBox.text = texts[n].text;
+    }
+}
