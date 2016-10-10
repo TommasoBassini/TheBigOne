e132bc258f564fea9392948d412e5fd58b7a39a0
dbga_30 <dbga_30@dbga.local>
2016-10-10 17:35:25 +0200

    Test Navmesh and Patroliing, OK!!

---
+++ Assets/Scripts/WIP/PlayerNavigation.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigation : MonoBehaviour {
+
+
+  
+    public NavMeshAgent navMeshAgent;
+    public Transform target;
+    public Transform target2;
+    Transform targetTmp;
+    public bool IsarrivedTarget = false;
+
+	// Use this for initialization
+	void Start () {
+        targetTmp = target;
+       
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll)
+        {
+           // IsarrivedTarget = !IsarrivedTarget;
+            if (IsarrivedTarget)
+            {
+                targetTmp = target;
+                IsarrivedTarget = false;
+
+            } else 
+            {
+                targetTmp = target2;
+                IsarrivedTarget = true;
+            }
+            
+            Debug.Log("ARRIVATO!");
+        } 
+      
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        navMeshAgent.SetDestination(targetTmp.transform.position);
+
+    
+    }
+
+
+}
