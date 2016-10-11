82b0b2dea00adb7d3ce60681ae295d7dd48febc4
dbga_34 <dbga_34@dbga.local>
2016-10-11 16:10:51 +0200

    Modular termial switch energy

---
+++ Assets/SwitchEnergyTerminal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchEnergyTerminal : MonoBehaviour
+{
+    public delegate void De_TerminalSwitch();
+    public De_TerminalSwitch terminalSwitch;
+
+
+    public bool isLight;
+    public bool isDoor;
+
+
+    public void ResetAllBool()
+    {
+        isLight = false;
+        isDoor = false;
+    }
+}
---
+++ Assets/SwitchPanelDoorButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPanelDoorButton : MonoBehaviour
+{
+    [Tooltip("Aggiungere il gameobject della porta")]
+    public DoorBaseScript door;
+    [Tooltip("Aggiungere il gameobject dove c'è lo script SwitchEnergyTerminal del terminale")]
+    public GameObject terminalMain;
+    private SwitchEnergyTerminal terminalSwitch;
+
+    void Start()
+    {
+        terminalSwitch = terminalMain.GetComponent<SwitchEnergyTerminal>();
+        terminalSwitch.terminalSwitch += DoorOn_Off;
+    }
+
+    public void SwitchDoorButton()
+    {
+        terminalSwitch.ResetAllBool();
+        terminalSwitch.isDoor = true;
+        terminalSwitch.terminalSwitch();
+    }
+
+    void DoorOn_Off()
+    {
+        switch (terminalSwitch.isDoor)
+        {
+            case (true):
+                {
+                    door.doorIsUnLocked = true;
+                    door.UnlockDoor();
+                    break;
+                }
+            case (false):
+                {
+                    door.doorIsUnLocked = false;
+                    door.UnlockDoor();
+                    break;
+                }
+        }
+    }
+}
