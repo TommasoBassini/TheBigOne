a22083e3120739a44d6663423fd5d93b5baddf72
dbga_34 <dbga_34@dbga.local>
2016-10-19 16:48:56 +0200

    no message

---
+++ Assets/PlayerStatus.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+class Permessi
+{
+    [Header ("Permessi medici")]
+    public bool isScannerMedic = false;
+    public bool isAemOssigeno = false;
+    public bool isIemMedico = false;
+}
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int medicLvl;
+    public int engineerLvl;
+    public int guardLvl;
+
+
+
+    public void MedicLvlUp()
+    {
+        medicLvl++;
+        switch (medicLvl)
+        {
+            default:
+                break;
+        }
+    }
+}
